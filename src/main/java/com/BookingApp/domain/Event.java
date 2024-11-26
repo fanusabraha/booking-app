95315b41a19f7eeef9bcab3291905fe169288879
fanus abraha <fanus.abraha@yahoo.com>
2024-11-26 12:10:18 -0600

    Update Event.java

--- src/main/java/com/BookingApp/domain/Event.java
+++ src/main/java/com/BookingApp/domain/Event.java
@@ -15,7 +15,18 @@ public class Event {
     // additional attributes for more filtered options
     /*
     * private Integer durationInHours (after it is being shown available on the date)
+    *private Boolean isPrivate;
+    *private Double averageRating;
+    * private List<Review> reviews;
+    * private Double budget;
     *
     * */
 
+    // additional attributes for contacting the user
+    /*
+    *private String contactEmail;
+    * private String contactPhone;
+    * private String description;
+    *
+    * */
 }
