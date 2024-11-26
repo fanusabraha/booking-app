6fae2ff4ef1f299e0ade04e43ea136d64dc0b5bd
fanus abraha <fanus.abraha@yahoo.com>
2024-11-25 22:28:39 -0600

    Event Class setting up

--- src/main/java/com/BookingApp/domain/Event.java
+++ src/main/java/com/BookingApp/domain/Event.java
@@ -1,4 +1,12 @@
 package com.BookingApp.domain;
 
 public class Event {
+    private String location;
+    private String continent;
+    private String country;
+    private Integer size;
+    public enum occasion {
+        WEDDING, BAPTISM, GRADUATION, ENGAGEMENT, BRIDAL_SHOWER, HOLIDAYS, ANNIVERSARIES, CONCERT, PARTY
+    };
+
 }
