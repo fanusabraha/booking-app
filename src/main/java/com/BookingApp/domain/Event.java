34845b82c9357709c79e81ab6ee92d1cd18ee61a
fanus abraha <fanus.abraha@yahoo.com>
2024-11-25 22:48:51 -0600

    Update Event

--- src/main/java/com/BookingApp/domain/Event.java
+++ src/main/java/com/BookingApp/domain/Event.java
@@ -1,12 +1,21 @@
 package com.BookingApp.domain;
 
+import java.time.LocalDate;
+
 public class Event {
     private String location;
-    private String continent;
     private String country;
-    private Integer size;
+    private String city;
+    private Integer numberOfVisitors;
+    private LocalDate date;
+
     public enum occasion {
         WEDDING, BAPTISM, GRADUATION, ENGAGEMENT, BRIDAL_SHOWER, HOLIDAYS, ANNIVERSARIES, CONCERT, PARTY
     };
+    // additional attributes for more filtered options
+    /*
+    * private Integer durationInHours (after it is being shown available on the date)
+    *
+    * */
 
 }
