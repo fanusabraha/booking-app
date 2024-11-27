60e0c92a03833c2a6b4bf1dd4508f05ec91ef886
fanus abraha <fanus.abraha@yahoo.com>
2024-11-26 21:44:32 -0600

    homepage html

--- src/main/java/com/BookingApp/web/EventController.java
+++ src/main/java/com/BookingApp/web/EventController.java
@@ -1,10 +1,21 @@
 package com.BookingApp.web;
 
+import com.BookingApp.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RestController("/event")
 public class EventController {
-    
+    @Autowired
+    EventService eventService;
+    @GetMapping("")
+    public String homePage(ModelMap model ){
+        
+        return "homepage";
+    }
+
 }
