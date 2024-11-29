c918d164ec3114df8bcd1d1b063bbdb482f86f2b
fanus abraha <fanus.abraha@yahoo.com>
2024-11-28 23:29:03 -0600

    Create User.java

---
+++ src/main/java/com/BookingApp/domain/User.java
@@ -0,0 +1,54 @@
+package com.BookingApp.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
