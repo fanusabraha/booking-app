2318648c006f0244aa41d87c15fc6d88588f4209
fanus abraha <fanus.abraha@yahoo.com>
2024-11-29 11:51:36 -0600

    Create UserDto.java

---
+++ src/main/java/com/BookingApp/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.BookingApp.dto;
+
+public class UserDto {
+    private long Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    
+}
