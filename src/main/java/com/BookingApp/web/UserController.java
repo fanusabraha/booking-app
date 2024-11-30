361b85b20c65303835f6b7e41062a4a8f693a179
fanus abraha <fanus.abraha@yahoo.com>
2024-11-29 20:50:45 -0600

    Update UserController.java

--- src/main/java/com/BookingApp/web/UserController.java
+++ src/main/java/com/BookingApp/web/UserController.java
@@ -3,10 +3,16 @@ package com.BookingApp.web;
 import com.BookingApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @Controller
-@RestController
+@RestController("/user")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/")
+    public String logIn(){
+        return"login";
+    }
 }
