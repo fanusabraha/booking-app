b874a8d60b83c6a81e5a7323a5adb5cd282f6124
fanus abraha <fanus.abraha@yahoo.com>
2024-12-01 18:01:40 -0600

    Update UserController.java

--- src/main/java/com/BookingApp/web/UserController.java
+++ src/main/java/com/BookingApp/web/UserController.java
@@ -7,17 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 @Controller
-@RestController("/user")
+@RestController("/user/login")
 public class UserController {
     @Autowired
     private UserService userService;
+    ArrayList<UserDto> users= new ArrayList<>();
 
     @GetMapping("/")
     public String logIn(ModelMap map){
         UserDto userDto = new UserDto();
         map.addAttribute("user",userDto);
         return"login";
+    }
+    @PostMapping("/")
+    public String logIn (@ModelAttribute("event") UserDto userDto){
+        users.add(userDto);
+        return"redirect:/event/";
     }
 }
