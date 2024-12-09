ec926669fed6eeb25bf91773175c90efcf99fb62
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-09 13:18:08 -0600

    Update locations.java

--- src/main/java/com/BookingApp/domain/locations.java
+++ src/main/java/com/BookingApp/domain/locations.java
@@ -1,5 +1,18 @@
 package com.BookingApp.domain;
 
 public class locations {
-    
+    private Long id;
+    private String location;
+    private String country;
+    private String city;
+    private Integer capacity;
+    private Integer price;
+    public enum inclusions {
+        MusicBand,
+        Catering,
+        Decorations,
+        
+
+    }
+
 }
