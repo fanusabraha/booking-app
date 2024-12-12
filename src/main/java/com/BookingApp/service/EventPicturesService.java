c745a669851d47abcc72538413a25ece9f93c647
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-11 21:18:37 -0600

    eventpicturesservice

--- src/main/java/com/BookingApp/service/EventPicturesService.java
+++ src/main/java/com/BookingApp/service/EventPicturesService.java
@@ -7,18 +7,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.apache.logging.log4j.util.Cast.cast;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @Service
-@RestController
-@RequestMapping("/")
 public class EventPicturesService {
     @Autowired
     EventPicturesRepository eventPicturesRepository;
-     public void SaveImagesToRepository (){
+     public void SaveImagesToRepository () {
+         String imagePath = "C:\\Users\\fanus\\OneDrive\\Desktop\\programming folders\\booking-app\\docs\\Hamburg.jpg";
+
+         byte[] imageBytes = null;
+         try {
+             imageBytes = Files.readAllBytes(new File(imagePath).toPath());
+
+         } catch (IOException e) {
+             System.out.println("Error reading image: " + e.getMessage());
+         }
          EventPictures eventPictures = new EventPictures();
-         eventPictures.setImageData();
+         eventPictures.setImageData(imageBytes);
          eventPictures.setId(1L);
-         eventPicturesRepository.save()
+         eventPicturesRepository.save(eventPictures);
      }
 }
---
+++ src/main/java/com/BookingApp/web/EventPicturesController.java
@@ -0,0 +1,7 @@
+package com.BookingApp.web;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventPicturesController {
+}
