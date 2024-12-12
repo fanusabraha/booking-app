c19c3e35e547e3dc72719b89541664263ec83a9b
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-11 19:14:10 -0600

    Update EventPicturesService.java

--- src/main/java/com/BookingApp/service/EventPicturesService.java
+++ src/main/java/com/BookingApp/service/EventPicturesService.java
@@ -1,15 +1,24 @@
 package com.BookingApp.service;
 
+import com.BookingApp.domain.EventPictures;
 import com.BookingApp.repository.EventPicturesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.apache.logging.log4j.util.Cast.cast;
 
 @Service
+@RestController
+@RequestMapping("/")
 public class EventPicturesService {
     @Autowired
     EventPicturesRepository eventPicturesRepository;
      public void SaveImagesToRepository (){
-         
+         EventPictures eventPictures = new EventPictures();
+         eventPictures.setImageData();
+         eventPictures.setId(1L);
          eventPicturesRepository.save()
      }
 }
