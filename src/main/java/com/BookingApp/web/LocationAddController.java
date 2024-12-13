ca0032c1c3b5abc3b1feaa58c433579d7e72531a
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-12 22:24:35 -0600

    update

--- src/main/java/com/BookingApp/web/LocationAddController.java
+++ src/main/java/com/BookingApp/web/LocationAddController.java
@@ -1,12 +1,14 @@
 package com.BookingApp.web;
 
 import com.BookingApp.domain.EventLocation;
+import com.BookingApp.dto.EventSearchDto;
 import com.BookingApp.repository.LocationRepository;
 import com.BookingApp.service.LocationAddService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,7 +24,9 @@ public class LocationAddController {
     return"addlocation";
     }
     @PostMapping("/new")
-    public String addLocation (){
+    public String addLocation (@ModelAttribute("eventLocation") EventLocation eventLocation){
+
+        locationAddService.saveLocation(eventLocation);
         return"redirect:";
     }
 }
