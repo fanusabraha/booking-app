a41c4b48b71b93638a1b5c0ea658f8effa9fe928
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-12 21:48:58 -0600

    addlocation

--- src/main/java/com/BookingApp/web/LocationAddController.java
+++ src/main/java/com/BookingApp/web/LocationAddController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/addLocation")
+@RequestMapping("/addlocation")
 public class LocationAddController {
     @Autowired
     LocationAddService locationAddService;
