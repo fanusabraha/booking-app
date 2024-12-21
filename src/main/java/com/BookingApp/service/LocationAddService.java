eb180a73db637cd9ef3e54ee4103d091fb84ebae
fanus abraha <150704985+fanusabraha@users.noreply.github.com>
2024-12-20 21:24:45 -0600

    summary onclick button

--- src/main/java/com/BookingApp/service/LocationAddService.java
+++ src/main/java/com/BookingApp/service/LocationAddService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
